54cd16cd5569c67f247b645dc876005cf8a0b31f
topdev0528 <top.dev0528@gmail.com>
2021-06-17 17:28:04 +0200

    Update BuySection component.

--- src/assets/jss/pages/Home/buySectionStyle.js
+++ src/assets/jss/pages/Home/buySectionStyle.js
@@ -6,18 +6,23 @@ const buySectionStyle = {
     position: 'relative',
   },
   starLightArea: {
-    positioin: 'absolute',
+    position: 'absolute',
     top: 0,
     left: 0,
     zIndex: 0,
-    marginTop: '-150px',
+    marginTop: '-400px',
     width: '100%',
     maxWidth: '515px',
   },
   container: {
     ...container,
-    marginTop: '-300px',
-    '@media (max-width: 600px)': { marginTop: '-200px' },
+  },
+  content: {
+    '@media (max-width: 600px)': { marginTop: '100px' },
+    '@media (min-width: 600px)': { marginTop: '130px' },
+    '@media (min-width: 768px)': { marginTop: '160px' },
+    '@media (min-width: 960px)': { marginTop: '200px' },
+    '@media (min-width: 1280px)': { marginTop: '237px' },
   },
   buyTitleArea: {
     display: 'flex',
--- src/pages/Home/BuySection.js
+++ src/pages/Home/BuySection.js
@@ -21,41 +21,43 @@ const BuySection = () => {
     <Box className={classes.buySectionArea}>
       <img src={StarLightImage} className={classes.starLightArea} />
       <Box className={classes.container}>
-        <Grid container display="flex" justify="space-between">
-          <Grid item xs={12} sm={12} md={3} lg={3}>
-            <Box display="flex" justifyContent="center" mb={4.5}>
-              <img src={FoxImage} className={classes.foxImage} />
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={12} md={8} lg={8}>
-            <Box className={classes.buyTitleArea}>
-              <Typography className={classes.buyTitle}>#HOW TO BUY</Typography>
-            </Box>
-            <Box mb={5}>
-              <Typography className={classes.text21}>
-                The only place to buy or sell <a className={classes.text21 + ' ' + classes.yellowText}>PLTX</a> or{' '}
-                <a className={classes.text21 + ' ' + classes.yellowText}>LOREM IPSUM</a> safely is on Uniswap. There are
-                some centralized exchanges that have listed SHIB, but CEXs have a history of locking funds. If you can,
-                it is safer to trade on a decentralized exchange like Uniswap or 1inch.
-                <br />
-                <br />
-                <a className={classes.text21 + ' ' + classes.yellowText}>LOREM IPSUM</a> IS NOT AVAILABLE YET, BUT WILL
-                BE ABLE TO ACQUIRE SOON.
-              </Typography>
-            </Box>
-            <Box display="flex" flexWrap="wrap" justifyContent="flex-start" alignItems="center" mb={9}>
-              <Box mr={4} mb={3}>
-                <Typography className={classes.text21 + ' ' + classes.whiteText}>
-                  See the Live Chart analytics:
+        <Box className={classes.content}>
+          <Grid container display="flex" justify="space-between">
+            <Grid item xs={12} sm={12} md={3} lg={3}>
+              <Box display="flex" justifyContent="center" mb={4.5}>
+                <img src={FoxImage} className={classes.foxImage} />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={8} lg={8}>
+              <Box className={classes.buyTitleArea}>
+                <Typography className={classes.buyTitle}>#HOW TO BUY</Typography>
+              </Box>
+              <Box mb={5}>
+                <Typography className={classes.text21}>
+                  The only place to buy or sell <a className={classes.text21 + ' ' + classes.yellowText}>PLTX</a> or{' '}
+                  <a className={classes.text21 + ' ' + classes.yellowText}>LOREM IPSUM</a> safely is on Uniswap. There
+                  are some centralized exchanges that have listed SHIB, but CEXs have a history of locking funds. If you
+                  can, it is safer to trade on a decentralized exchange like Uniswap or 1inch.
+                  <br />
+                  <br />
+                  <a className={classes.text21 + ' ' + classes.yellowText}>LOREM IPSUM</a> IS NOT AVAILABLE YET, BUT
+                  WILL BE ABLE TO ACQUIRE SOON.
                 </Typography>
               </Box>
-              <Button title="PLTX" color="indigo" />
-            </Box>
-            <Box mb={9} className={classes.buttonArea}>
-              <Button title="BUY PLTX" color="yellow" />
-            </Box>
+              <Box display="flex" flexWrap="wrap" justifyContent="flex-start" alignItems="center" mb={9}>
+                <Box mr={4} mb={3}>
+                  <Typography className={classes.text21 + ' ' + classes.whiteText}>
+                    See the Live Chart analytics:
+                  </Typography>
+                </Box>
+                <Button title="PLTX" color="indigo" />
+              </Box>
+              <Box mb={9} className={classes.buttonArea}>
+                <Button title="BUY PLTX" color="yellow" />
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Box>
     </Box>
   )
